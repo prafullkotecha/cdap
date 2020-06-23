527f3862e98f27673379d7f7e7ebeb68bf7b1a9e
vinisha <vinishashah@google.com>
2020-06-23 15:47:40 -0700

    [CDAP-16754] Adding support for representing logical types as string in preview

--- cdap-formats/src/main/java/io/cdap/cdap/format/io/JsonStructuredRecordDatumReader.java
+++ cdap-formats/src/main/java/io/cdap/cdap/format/io/JsonStructuredRecordDatumReader.java
@@ -24,12 +24,19 @@ import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.common.io.Decoder;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link StructuredRecordDatumReader} that decodes from Json.
@@ -53,12 +60,14 @@ public class JsonStructuredRecordDatumReader extends StructuredRecordDatumReader
   );
 
   private final boolean fieldNameIgnoreCase;
+  private final boolean logicalTypeAsString;
+  private final boolean isPreview;
 
   /**
    * Constructor that treats field names case sensitive.
    */
   public JsonStructuredRecordDatumReader() {
-    this(false);
+    this(false, false, false);
   }
 
   /**
@@ -67,7 +76,20 @@ public class JsonStructuredRecordDatumReader extends StructuredRecordDatumReader
    * @param fieldNameIgnoreCase {@code true} to have case sensitive field names.
    */
   public JsonStructuredRecordDatumReader(boolean fieldNameIgnoreCase) {
+    this(fieldNameIgnoreCase, false, false);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param fieldNameIgnoreCase {@code true} to have case sensitive field names.
+   * @param logicalTypeAsString {@code true} to read string values of logical type fields.
+   * @param isPreview {@code true} to have logical type field values as string for preview
+   */
+  public JsonStructuredRecordDatumReader(boolean fieldNameIgnoreCase, boolean logicalTypeAsString, boolean isPreview) {
     this.fieldNameIgnoreCase = fieldNameIgnoreCase;
+    this.logicalTypeAsString = logicalTypeAsString;
+    this.isPreview = isPreview;
   }
 
   @Override
@@ -153,6 +175,43 @@ public class JsonStructuredRecordDatumReader extends StructuredRecordDatumReader
 
     throw new IOException(String.format("No matching schema found for union type: %s for token: %s", unionSchema,
                                         token));
+  }
+
+  @Override
+  protected Object decode(Decoder decoder, Schema schema) throws IOException {
+    Schema nonNullableSchema = schema.isNullable() ? schema.getNonNullable() : schema;
+    Schema.LogicalType logicalType = nonNullableSchema.getLogicalType();
+    if (logicalTypeAsString && logicalType != null) {
+      String decodedString = decoder.readString();
+      if (isPreview) {
+        // For preview always return a string. CDAP-16754 - Needed so that preview displays logical type as string.
+        return decodedString;
+      }
+      switch (logicalType) {
+        case DATE:
+          return Math.toIntExact(LocalDate.parse(decodedString, DateTimeFormatter.ISO_LOCAL_DATE).toEpochDay());
+        case TIME_MILLIS:
+          LocalTime localTimeMillis = LocalTime.parse(decodedString, DateTimeFormatter.ISO_LOCAL_TIME);
+          return Math.toIntExact(TimeUnit.NANOSECONDS.toMillis(localTimeMillis.toNanoOfDay()));
+        case TIME_MICROS:
+          LocalTime localTimeMicros = LocalTime.parse(decodedString, DateTimeFormatter.ISO_LOCAL_TIME);
+          return TimeUnit.NANOSECONDS.toMicros(localTimeMicros.toNanoOfDay());
+        case TIMESTAMP_MILLIS:
+          ZonedDateTime zonedDateTimeMillis = ZonedDateTime.parse(decodedString, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+          Instant instantMillis = zonedDateTimeMillis.toInstant();
+          long millis = TimeUnit.SECONDS.toMillis(instantMillis.getEpochSecond());
+          return Math.addExact(millis, TimeUnit.NANOSECONDS.toMillis(instantMillis.getNano()));
+        case TIMESTAMP_MICROS:
+          ZonedDateTime zonedDateTimeMicros = ZonedDateTime.parse(decodedString, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+          Instant instantMicros = zonedDateTimeMicros.toInstant();
+          long micros = TimeUnit.SECONDS.toMicros(instantMicros.getEpochSecond());
+          return Math.addExact(micros, TimeUnit.NANOSECONDS.toMicros(instantMicros.getNano()));
+        case DECIMAL:
+          BigDecimal bigDecimal = new BigDecimal(decodedString);
+          return bigDecimal.unscaledValue().toByteArray();
+      }
+    }
+    return super.decode(decoder, schema);
   }
 
   private JsonReader getJsonReader(Decoder decoder) {
